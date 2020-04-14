6543f0d3caf3f2847e90679e3ebcca2f03bb484b
AaronMartin <34367282+MartinAaron@users.noreply.github.com>
2020-04-14 22:31:03 +0800

    FisrtCommit

---
+++ Fluent.Entity/BaseEntity/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluent.Entity.BaseEntity
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public double Double { get; set; }
+        public float Float { get; set; }
+    }
+}
