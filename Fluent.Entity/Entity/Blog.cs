1acee73cc2bbf98d7385ded1be906824f81a8cdc
xhp <fush1.core@gmail.com>
2020-05-19 09:49:38 +0800

    测试

--- Fluent.Entity/Entity/Blog.cs
+++ Fluent.Entity/Entity/Blog.cs
@@ -13,5 +13,6 @@ namespace Fluent.Entity.Entity
         public string Url { get; set; }
         public DateTime? CreateTime { get; set; }
         public double Double { get; set; }
+        public string Address{get;set;}
     }
 }
